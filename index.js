672c250ae6de6674b76a28b59e6b3c5e5883a56d
Stephanelle Rachwan <stephanellerachwan@gmail.com>
2022-06-25 12:38:30 +0300

    creating our connection with the database on Atlas

---
+++ index.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const userRouter = require("./src/user");
+
+const DB_CONNECT = process.env.DB_CONNECT || "";
+mongoose.connect(DB_CONNECT, () => console.log("connected to db"));
+
+const app = express();
+app.use(cors());
+
+app.use("/api/user", userRouter);
+app.listen(3000, () => console.log("Server Running"));
